09c0365e7021fc653acf6616c90f37042c8cc38f
Aditya Rameshwar Bahe <adityabahe@DESKTOP-U6R63L5.localdomain>
2025-06-13 12:39:59 +0000

    used vec3.h and color.h in main

--- output_image.cpp
+++ output_image.cpp
@@ -1,4 +1,6 @@
 #include<iostream>
+#include "color.h"
+#include "vec3.h"
 using namespace std;
 
 int main() {
@@ -13,15 +15,8 @@ int main() {
    for (int j = 0; j < image_height; ++j) {
         std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
         for (int i = 0; i < image_width; i++) {
-            auto r = double(i) / (image_width-1);
-            auto g = double(j) / (image_height-1);
-            auto b = 0.0;
-
-            int ir = int(255.999 * r);
-            int ig = int(255.999 * g);
-            int ib = int(255.999 * b);
-
-            std::cout << ir << ' ' << ig << ' ' << ib << ' ';
+            color pixel_color = color(double(i)/(image_width - 1), double(j) / (image_height - 1), 0);
+            write_color(cout, pixel_color);
         }
             cout << "\n";
     }
