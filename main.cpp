9676910752f22e17d5c31961b06e060ac5278392
Aditya Rameshwar Bahe <adityabahe@DESKTOP-U6R63L5.localdomain>
2025-06-14 03:35:16 +0000

    added a module to check if a sphere created got hit by ray

--- main.cpp
+++ main.cpp
@@ -4,10 +4,23 @@
 #include "ray.h"
 using namespace std;
 
+
+bool hit_sphere(const point3& center, double radius, const ray& r){
+    auto oc = center - r.origin();
+    auto a = dot(r.direction(), r.direction());
+    auto b = -2 * dot(r.direction(), oc);
+    auto c = dot(oc,oc) - radius * radius;
+
+    auto discriminant = b*b - 4*a*c;
+    return (discriminant >= 0);
+}
+
 color color_ray(const ray& r){
+    if(hit_sphere(point3(0,0,-1) , 0.5, r)) return color(1, 0, 0);
+
     vec3 unit_dir = unit_vector(r.direction());
     auto a = 0.5 * (unit_dir.y() + 1);
-    return (1-a)*color(1.0, 1.0, 1.0) + a*color(0.5,0.7,1.0);
+    return (1 - a) * color(1.0, 1.0, 1.0) + a*color(0.5, 0.7, 1.0);
 }
 
 int main() {
