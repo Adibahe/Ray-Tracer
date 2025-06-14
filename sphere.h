fe17b8bdc096325e6b445cc38ab10c46d24ead65
Aditya Rameshwar Bahe <adityabahe@DESKTOP-U6R63L5.localdomain>
2025-06-14 11:27:16 +0000

    added a sphere.h for determining a hit to any sphere object

---
+++ sphere.h
@@ -0,0 +1,44 @@
+#ifndef SPHERE_H
+#define SPHERE_H
+
+#include "hittable.h"
+using namespace std;
+
+class sphere : public hittable{
+    
+    private:
+        point3 center;
+        double radius;
+
+    public:
+        sphere(const point3& center, double radius) : center(center), radius(fmax(0,radius)){}
+        
+        bool hit(const ray& r, double ray_tmin, double ray_tmax, hit_record& rec ){
+            auto oc = center - r.origin();
+            auto a = dot(r.direction(), r.direction());
+            auto h = dot(r.direction(), oc);
+            auto c = dot(oc,oc) - radius * radius;
+
+            auto discriminant = h*h - a*c;
+            if(discriminant < 0) return false;
+            
+            auto sqtrd = sqrt(discriminant);
+
+            // find root
+
+            auto root = (h - sqtrd)/a;
+            if(root <= ray_tmin || ray_tmax <= root){
+                root = (h + sqtrd)/a;
+                if(root <= ray_tmin || ray_tmax <= root)
+                    return false;
+            }
+
+            rec.t = root;
+            rec.p = r.at(rec.t);
+            rec.normal = (rec.p - center)/radius;
+
+            return true;
+        }
+};
+
+#endif
