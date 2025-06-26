9f900288123b828a3e02ef55bfc108524dc82f53
Aditya Rameshwar Bahe <adityabahe@DESKTOP-U6R63L5.localdomain>
2025-06-26 03:04:12 +0000

    added schlick approximation for varing reflectivity of glass

--- material.h
+++ material.h
@@ -73,7 +73,7 @@ class dielectric : public material{
             double sin_theta = std::sqrt(1.0 - cos_theta*cos_theta);
 
             vec3 direction;
-            if(ri * sin_theta > 1.0){
+            if(ri * sin_theta > 1.0 || reflectance(cos_theta, ri) > random_double()){
                 direction = reflection(unit_dir, rec.normal);
             }
             else{
@@ -86,6 +86,13 @@ class dielectric : public material{
 
     private:
         double refractive_index;
+
+        static double reflectance(double cosine, double refraction_index) {
+            // Use Schlick's approximation for reflectance.
+            auto r0 = (1 - refraction_index) / (1 + refraction_index);
+            r0 = r0*r0;
+            return r0 + (1-r0)*std::pow((1 - cosine),5);
+        }
 };
 
 #endif
